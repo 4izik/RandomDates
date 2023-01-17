048597c59b2bbc89af731469fb4ca8cef4df07ab
Andrey91 <mistaandrey@icloud.com>
2023-01-17 11:30:37 +0500

    Initial project

---
+++ RandomDates/RandomDatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomDatesApp.swift
+//  RandomDates
+//
+//  Created by Andrey Piskunov on 16.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct RandomDatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
